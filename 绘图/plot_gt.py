18cbf61f85e6c4c7fee63c3ae6bde152d9ab7513
ShuGuoJ <2462230948@qq.com>
2021-05-10 21:45:10 +0800

    Add files via upload

--- 绘图/plot_gt.py
+++ 绘图/plot_gt.py
@@ -1,3 +1,33 @@
+<<<<<<< HEAD
+'''绘画groundtruth map'''
+from matplotlib import pyplot as plt
+from scipy.io import loadmat
+import numpy as np
+import glob
+from utils import plot_map
+import configparser
+
+
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('dataInfo.ini')
+    path = glob.glob('gt\\*.mat')
+    for p in path:
+        m = loadmat(p)
+        name = p.split('\\')[-1].split('.')[0].split('_')[0]
+        key = config.get(name, 'gt_key')
+        gt = m[key]
+        h, w = gt.shape
+        map = plot_map(gt)
+        plt.figure(figsize=(w/100.0, h/100.0))
+        plt.imshow(map, aspect='equal')
+        plt.axis('off')
+        plt.subplots_adjust(top=1, bottom=0, left=0, right=1, wspace=0, hspace=0)
+        plt.margins(0, 0)
+        plt.savefig('gt/{}.eps'.format(name.split('_')[0]))
+        plt.close()
+        print('*'*5 + 'FINISH {}'.format(name) + '*'*5)
+=======
 '''绘画groundtruth map'''
 from matplotlib import pyplot as plt
 from scipy.io import loadmat
@@ -27,3 +57,4 @@ if __name__ == '__main__':
         plt.savefig('gt/{}.eps'.format(name.split('_')[0]))
         plt.close()
         print('*'*5 + 'FINISH {}'.format(name) + '*'*5)
+>>>>>>> 2b422f73497afd1f4ee5fe52321761de168e9451
--- 绘图/plot_rgb.py
+++ 绘图/plot_rgb.py
@@ -1,3 +1,46 @@
+<<<<<<< HEAD
+from utils import rgb
+from matplotlib import pyplot as plt
+from scipy.io import loadmat
+import numpy as np
+import argparse
+import os
+import configparser
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='绘画高光谱图像的RGB图像')
+    parser.add_argument('--name', type=str, default='PaviaU',
+                        help='DATASET NAME')
+    arg = parser.parse_args()
+    config = configparser.ConfigParser()
+    config.read('dataInfo.ini')
+    dataset_name = arg.name
+    m = loadmat('data/{0}/{0}.mat'.format(dataset_name))
+    data = m[config.get(arg.name, 'data_key')]
+    data = data.astype(np.float)
+    b_0 = config.getint(arg.name, 'band_begin')
+    b_n = config.getint(arg.name, 'band_end')
+    n = config.getint(arg.name, 'band')
+    d = (b_n - b_0) / (n - 1)
+    r, g, b = rgb(b_0 ,d)
+    img = data[..., (r, g, b)]
+    h, w = img.shape[:2]
+    data = img.reshape((h*w, -1))
+    data -= np.min(data, axis=0)
+    data /= (np.max(data, axis=0) + 1e-8)
+    img = data.reshape((h, w, -1))
+    img *= 255
+    img = img.astype(np.uint8)
+    plt.figure(figsize=(w / 100.0, h / 100.0))
+    plt.imshow(img, aspect='equal')
+    plt.axis('off')
+    plt.subplots_adjust(top=1, bottom=0, left=0, right=1, wspace=0, hspace=0)
+    plt.margins(0, 0)
+    # plt.show()
+    if not os.path.exists('rgb'):
+        os.mkdir('rgb')
+    plt.savefig('rgb/{}_rgb.pdf'.format(dataset_name))
+=======
 from utils import rgb
 from matplotlib import pyplot as plt
 from scipy.io import loadmat
@@ -59,4 +102,5 @@ if __name__ == '__main__':
     if not os.path.exists('rgb'):
         os.mkdir('rgb')
     plt.savefig('rgb/{}_rgb.pdf'.format(dataset_name))
+>>>>>>> 2b422f73497afd1f4ee5fe52321761de168e9451
     print('*'*5 + 'FINISH PLOT {}'.format(dataset_name) + '*'*5)
